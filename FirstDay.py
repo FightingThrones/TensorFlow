36c10f3fbe64a54b06d35ff6ec8cf4f6e9e02c5a
FightingYang <yang18885672964@gmail.com>
2019-03-09 00:21:30 +0800

    第一天学习完整笔记

--- FirstDay.py
+++ FirstDay.py
@@ -1,8 +1,8 @@
 """
- 目的：    Tensorflow学习代码练习
+ 目的：     Tensorflow学习代码练习
  @Author:  Thrones
- GitHub:
- Email:
+ GitHub:   https://github.com/FightingThrones/TensorFlow
+ Email:    yang18885672964@gmail.com
 """
 
 #tensorflow安装是否成功测试代码
@@ -54,19 +54,88 @@
 # 7.RGB bgr
 # 8.bgr b g
 
-#像素读取写入操作
-import cv2
-img=cv2.imread('images/9.jpg',1)
-#像素的读取返回存入一个元组中
-#b g r 三种颜色通道：蓝色 绿色 红色
-(b,g,r)=img[100,100]
-#打印图片的[100,100]这个坐标点的b g r 值
-print(b,g,r)
+# #像素读取写入操作
+# import cv2
+# img=cv2.imread('images/9.jpg',1)
+# #像素的读取返回存入一个元组中
+# #b g r 三种颜色通道：蓝色 绿色 红色
+# (b,g,r)=img[100,100]
+# #打印图片的[100,100]这个坐标点的b g r 值
+# print(b,g,r)
+#
+# #在图片上绘制一条直线
+# #[10 ,100]--[110,100]
+# for i in range(1,100):
+#     img[10+i,100]=[0,0,255]
+# #显示图片
+# cv2.imshow('image',img)
+# cv2.waitKey(0)
 
-#在图片上绘制一条直线
-#[10 ,100]--[110,100]
-for i in range(1,100):
-    img[10+i,100]=[0,0,255]
-#显示图片
-cv2.imshow('image',img)
-cv2.waitKey(0)
+# #tensorflow常量变量定义
+# import tensorflow as tf
+# data1=tf.constant(2,dtype=tf.int32)
+# data2=tf.Variable(10,name='var')
+# print(data1)
+# print(data2)
+# sess=tf.Session()
+# print(sess.run(data1))
+# #变量不初始化会报错
+# #给变量初始化
+# init=tf.global_variables_initializer()
+# sess.run(init)
+# print(sess.run(data2))
+#
+# #sess.close()
+# #或者
+# init=tf.global_variables_initializer()
+# sess=tf.Session()
+# with sess:
+#     sess.run(init)
+#     print(sess.run(data2))
+
+#tensorflow运算实质笔记
+#TensorFlow的实质:张量tensor+计算图graphs
+#tensor 数据
+#op 操作
+#graphs  数据操作
+#session 会话图
+
+
+# #常量变量四则运算
+# import tensorflow as tf
+# data1=tf.constant(6)
+# data2=tf.constant(2)
+# dataAdd=tf.add(data1,data2)
+# dataSub=tf.subtract(data1,data2)
+# dataMul=tf.multiply(data1,data2)
+# dataDiv=tf.divide(data1,data2)
+# with tf.Session() as sess:
+#     print(sess.run(dataAdd))
+#     print(sess.run(dataSub))
+#     print(sess.run(dataMul))
+#     print(sess.run(dataDiv))
+# print('End!')
+
+#变量变量四则运算
+import tensorflow as tf
+data1=tf.constant(6)
+data2=tf.Variable(2)
+dataAdd=tf.add(data1,data2)
+#dataAdd ->data2
+dataCopy=tf.assign(data2,dataAdd)
+dataSub=tf.subtract(data1,data2)
+dataMul=tf.multiply(data1,data2)
+dataDiv=tf.divide(data1,data2)
+init=tf.global_variables_initializer()
+with tf.Session() as sess:
+    print(sess.run(init))
+    print(sess.run(dataAdd))
+    print(sess.run(dataSub))
+    print(sess.run(dataMul))
+    print(sess.run(dataDiv))
+    print('sess.run(dataCopy)',sess.run(dataCopy))
+    print('dataCopy.eval()',dataCopy.eval())
+    print('tf.get_default_session()',tf.get_default_session().run(dataCopy))
+print('End!')
+
+
